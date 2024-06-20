b4504697cf23e09fe5992fe55584807f370ee0af
AbdAlrahim Zakaria <abood.zakaryaa@gmail.com>
2024-06-20 21:18:36 +0300

    configured environment variables, adds cleanup

--- app/database.py
+++ app/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:70336165@localhost/fastapi"
+from .config import settings
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/oauth2.py
+++ app/oauth2.py
@@ -3,11 +3,12 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer
-from . import schemas, database, models
+from . import database, models
+from .config import settings
 
-SECRET_KEY = "5f7bd31f7393b691ef07b535d9fd5d1d2e11573966f8820c01c9bc67c27c34c9"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.jwt_secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
